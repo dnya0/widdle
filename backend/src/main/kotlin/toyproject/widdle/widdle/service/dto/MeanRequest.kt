3067b001e1c0ce6677ea663d9a0f8aed830c6b19
dnya0 <dsd932@naver.com>
2025-09-15 23:59:25 +0900

    feat: add SearchService

---
+++ backend/src/main/kotlin/toyproject/widdle/widdle/service/dto/MeanRequest.kt
@@ -0,0 +1,11 @@
+package toyproject.widdle.widdle.service.dto
+
+class MeansRequest(private val key: String, private val q: String?) {
+    private val part: String? = "word"
+    private val translated: String? = "n"
+    private val advanced: String? = "y"
+    private val method: String? = "exact"
+
+    val parameter: String
+        get() = "?key=$key&q=$q&part=$part&translated=$translated&advanced=$advanced&method=$method"
+}
