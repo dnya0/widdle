14b6ca5df854a509202cbc24f9c3a49d796240ee
dnya0 <annay01794@gmail.com>
2025-09-18 00:00:06 +0900

    chore: add webflux

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
     //kotlin
