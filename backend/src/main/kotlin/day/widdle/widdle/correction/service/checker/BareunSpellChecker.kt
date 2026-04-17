b4698358c83a67f331645de3d4b4f06a96798242
dnya0 <annay01794@gmail.com>
2026-04-18 08:23:14 +0900

    refactor: remove sslContext shadowing in Bareun client

--- backend/src/main/kotlin/day/widdle/widdle/correction/service/checker/BareunSpellChecker.kt
+++ backend/src/main/kotlin/day/widdle/widdle/correction/service/checker/BareunSpellChecker.kt
@@ -59,22 +59,19 @@ class BareunSpellChecker(
 
     private fun isProfileDevThanSetSSLIgnore(builder: WebClient.Builder): WebClient.Builder {
         val isDevProfile = env.acceptsProfiles(Profiles.of("dev"))
-
-        return if (isDevProfile) {
-            val sslContext = SslContextBuilder.forClient()
+        val appliedSslContext = if (isDevProfile) {
+            val devSslContext = SslContextBuilder.forClient()
                 .trustManager(InsecureTrustManagerFactory.INSTANCE)
                 .build()
-
-            val httpClient = HttpClient.create()
-                .secure { t -> t.sslContext(sslContext) }
-
-            builder.clientConnector(ReactorClientHttpConnector(httpClient))
+            devSslContext
         } else {
-            val httpClient = HttpClient.create()
-                .secure { t -> t.sslContext(sslContext) }
-
-            builder.clientConnector(ReactorClientHttpConnector(httpClient))
+            sslContext
         }
+
+        val httpClient = HttpClient.create()
+            .secure { t -> t.sslContext(appliedSslContext) }
+
+        return builder.clientConnector(ReactorClientHttpConnector(httpClient))
     }
 
     private fun handleErrorResponse(clientResponse: ClientResponse): Mono<Throwable> =
